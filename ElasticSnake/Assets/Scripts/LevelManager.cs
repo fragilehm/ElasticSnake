72c61f0e0cd3d5acd89dfb28aac9de3d0639a562
fragilehm <fragilehm2411@gmail.com>
2016-12-28 13:32:30 +0600

    Menu

---
+++ ElasticSnake/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    
+	// Use this for initialization
+	public void Play() {
+        SceneManager.LoadScene("Play");
+	}
+	public void Instructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+	// Update is called once per frame
+	public void Quit () {
+        Application.Quit();
+	}
+}
